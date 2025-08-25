697840f9d656cfdc1cd3f0f360668146c106f622
Kioshy <kioshy@gmail.com>
2025-08-24 23:51:22 -0300

    feat: boot da aplicação

---
+++ src/main/java/br/com/desafio/banco/AplicacaoBanco.java
@@ -0,0 +1,3 @@
+package br.com.desafio.banco;
+import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication public class AplicacaoBanco { public static void main(String[] args){ SpringApplication.run(AplicacaoBanco.class, args); } }
